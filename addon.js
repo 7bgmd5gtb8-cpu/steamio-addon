a8520b930aaaf82fcbd5470eefd9012af5081616
7bgmd5gtb8-cpu <7bgmd5gtb8@privaterelay.appleid.com>
2026-06-11 13:48:32 -0600

    Add files via upload

---
+++ addon.js
@@ -0,0 +1,24 @@
+const { addonBuilder, serveHTTP } = require('stremio-addon-sdk');
+
+const builder = new addonBuilder({
+    id: 'org.myexampleaddon',
+    version: '1.0.0',
+    name: 'Simple Example Addon',
+    description: 'A basic addon for testing',
+    resources: ['stream'],
+    types: ['movie'],
+    idPrefixes: ['tt']
+});
+
+builder.defineStreamHandler(function(args) {
+    if (args.type === 'movie' && args.id === 'tt1254207') {
+        const stream = { 
+            url: 'http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_30fps_normal.mp4' 
+        };
+        return Promise.resolve({ streams: [stream] });
+    } else {
+        return Promise.resolve({ streams: [] });
+    }
+});
+
+serveHTTP(builder.getInterface(), { port: 7000 });
